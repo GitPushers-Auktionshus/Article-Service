65a18c598231d6484379984858745f47a1b9611f
Jacob Kaae <jacobkaae0511@gmail.com>
2023-05-15 13:32:49 +0200

    Tilfjet fixes

---
+++ ArticleServiceAPI/Service/IArticleRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleServiceAPI.Service
+{
+	public interface IArticleRepository
+	{
+		public Task<IActionResult> AddArticle(Object x);
+
+        public Task<IActionResult> DeleteArticle(Object x);
+
+
+    }
+
+    public class MongoRepository : IArticleRepository
+    {
+        public Task<IActionResult> AddArticle(object x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteArticle(object x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
